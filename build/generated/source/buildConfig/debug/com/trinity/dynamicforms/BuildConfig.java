4339eab5553d6a60f0ad749557369b29d645a0aa
mompi <mompi.devi@trinityapplab.co.in>
2020-12-09 18:10:55 +0530

    Added theme fix

--- build/generated/source/buildConfig/debug/com/trinity/dynamicforms/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/trinity/dynamicforms/BuildConfig.java
@@ -5,8 +5,9 @@ package com.trinity.dynamicforms;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "com.trinity.dynamicforms";
+  public static final String APPLICATION_ID = "com.trinity.dynamicforms";
   public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
 }
